ad294e2f91b53782ccf3f46dde3263db02df6541
Pramod Naidu G V <gvpramod.1992@hotail.com>
2023-10-27 21:01:37 +0530

    Test 1

--- main.py
+++ main.py
@@ -1,2 +1,60 @@
-print('test git repo')
-print('Test line 2')
+# print('test git repo')
+# print('Test line 2')
+
+# CLASS ON 26TH OCTOBER 2023
+
+# x = int(input('Enter Number:'))
+# y = int(input('Enter Another Number:'))
+# Sum = x+y
+# Multi = x*y
+# print(Sum)
+# print(Multi)
+# print(Sum+Multi)
+
+# def addition(arg1,arg2):
+#     return arg1 + arg2
+# def multiplication(arg1,arg2):
+#     return arg1*arg2
+# x = int(input('Enter Number:'))
+# y = int(input('Enter Another Number:'))
+#
+# Sum = addition(x,y)
+# Multi = multiplication(x,y)
+# Result = addition(Sum,Multi)
+#
+# print(Sum)
+# print(Multi)
+# print(Result)
+
+#Code Re-Usability
+
+# x = 0 #global variable
+# y = 5000
+# def display():
+#     y = 1000 #local variable
+#     print(x)
+#     print(y)
+#
+# print(x)
+# display()
+# print(y)
+
+# x = 0 #global variable
+# def display():
+#     y = 1000 #local variable
+#     print(x)
+#     print(y)
+#     return y
+#
+# print(x)
+# d = display()
+# print(d)
+
+# x = 0 #global variable
+# def display():
+#     global x
+#     x = 1000 #local variable
+#     print(x)
+#
+# display()
+# print(x)

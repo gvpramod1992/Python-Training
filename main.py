59bb82da83929f79f64905df839ac17aaba02bda
skillforjobs <skillforjobs@gmail.com>
2023-10-28 09:28:04 +0530

    Restore inv management code

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
 print('test git repo')
 print('Test line 2')
 
+print('test git repo')
+print('Test line 2')
+# database -- hard disk -- file system
+
+# Inventory management
+# 1. AddItem, 2. RemoveItem, 3. DisplayItems
+
+# Add 1-10 items
+# items = []
+#
+while True:
+    print('Available options: \n1. AddItem \n2. DisplayItems \n3.Exit')
+    option = int(input('Choose an option: '))
+    if option == 1: # AddItem
+        item = input('Enter item name: ')
+        # items.append(item)
+        # write to a file
+            # file name
+            # location
+            # write
+        # open - python function to write/read from a file
+
+        fp = open('inventory.txt', 'a')
+        fp.write()
+        fp.write(item)
+        fp.close()
+    elif option == 2:
+        pass
+        # print(items)
+    elif option == 3:
+        break
+    else:
+        print('Invalid option')
+
+
 
 
 # x = 0 #global variable
